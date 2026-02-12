33a90b1ce7c78ec7332559da952a30d57c2b8650
Kabilan K <110909765+Kabi003@users.noreply.github.com>
2026-02-12 21:08:13 +0530

    Add files via upload

---
+++ LONGEST_COMMON_PREFIX.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        
+        Arrays.sort(strs);
+        
+        StringBuilder result = new StringBuilder();
+        
+        char[] first = strs[0].toCharArray();
+        char[] last = strs[strs.length - 1].toCharArray();
+        
+        for (int i = 0; i < first.length && i < last.length; i++) {
+            
+            if (first[i] != last[i]) {
+                break;
+            }
+            
+            result.append(first[i]);
+        }
+        
+        return result.toString();
+    }
+}
